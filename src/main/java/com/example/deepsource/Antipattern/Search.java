377ed6002763781f673992334abc5e3db333e718
brockpape <mpapenfuss@brockhaus-ag.de>
2023-08-16 11:45:19 +0200

    Anitpattern Search

---
+++ src/main/java/com/example/deepsource/Antipattern/Search.java
@@ -0,0 +1,33 @@
+package com.example.deepsource.Antipattern;
+
+import java.util.Arrays;
+
+public class Search {
+
+    public int search() {
+        String[] candidates = getCandidates();
+        String searchingFor = "The Lost Boys";
+        int found = -1;
+        for (int i = 0; i < candidates.length; i++) { // flat tire
+            if (candidates[i].equals(searchingFor)) {
+                found = i;
+            }
+        }
+        return found;
+    }
+
+    public int betterApproch() {
+        int found = -1;
+        String searchingFor = "The Lost Boys";
+        String[] candidates = getCandidates();
+        Arrays.sort(candidates); // start of "better" approach
+        found = Arrays.binarySearch(candidates, searchingFor);
+        return found;
+    }
+
+    private String[] getCandidates() {
+        String[] strings = {"1", "2", "The Lost Boys"};
+        return strings;
+    }
+
+}

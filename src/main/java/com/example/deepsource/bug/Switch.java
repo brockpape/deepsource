3f8a366a135c79d2a119abfbb40720a1f772f18d
brockpape <mpapenfuss@brockhaus-ag.de>
2023-08-16 11:27:45 +0200

    Switch Fail

---
+++ src/main/java/com/example/deepsource/bug/Switch.java
@@ -0,0 +1,20 @@
+package com.example.deepsource.bug;
+
+public class Switch {
+
+    public static void switchCasePrimer(int caseIndex) {
+        switch (caseIndex) {
+            case 0:
+                System.out.println("Zero");
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("Default");
+        }
+    }
+
+}
